1dc73e19e57b992c57076d485422dbaf5d6bd59c
SudhanvaRD <sudhanva.d@intechadditive.com>
2023-04-25 10:20:24 +0530

    wa

--- sum.js
+++ sum.js
@@ -11,8 +11,10 @@ function getTime(volume,productivity)
 function divide(a,b){
     return a/b 
 }
-function divide(a,b){
-    return a/b 
+
+
+function multiply(a,b){
+    return a*b 
 }
 function multiply(a,b){
     return a*b 
